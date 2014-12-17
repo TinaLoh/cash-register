3ff85080467400d35dcca66bf0e9b6b58d7b01ac
TinaLoh <tinalola@gmail.com>
2014-12-17 09:42:54 -0800

    added tests and methods

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,5 +1,19 @@
 class CashRegister
-  def total
-    0
+
+  def initialize
+    @total = 0
   end
+
+  def total
+  @total
+  end
+
+  def purchase(item_price)
+    @total += item_price.to_f
+  end
+
+  def pay(amount_paid)
+    amount_paid.to_f - @total
+  end
+
 end
--- spec/cash_register_spec.rb
+++ spec/cash_register_spec.rb
@@ -1,13 +1,40 @@
 require 'cash_register'
 
 describe CashRegister do
-  describe "#total" do
+
+  describe "#total" do  # the pound sign in describe represents a method
     it 'starts at 0' do
       cash_register = CashRegister.new
 
       total = cash_register.total
 
       expect(total).to eq(0)
+
+    end
+  end
+
+
+  describe "#purchase" do
+    it 'takes a floating number and adds that to the total' do
+
+      cash_register = CashRegister.new
+
+      total = cash_register.purchase(5.99)
+
+      expect(total).to eq(5.99)
+    end
+  end
+
+  describe "#pay" do
+    it 'takes one floating number for how much is paid. It should return how much change is given' do
+
+      cash_register = CashRegister.new
+
+      cash_register.purchase(13.99)
+      change = cash_register.pay(15.99)
+
+      expect(change).to eq(2.00)
+
     end
   end
 end
